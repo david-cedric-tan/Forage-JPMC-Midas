e03b6e3f177a31c43ab7e1a8cee07593ec8e0d95
ANTiDOTE-21 <dtan7448@uni.sydney.edu.au>
2025-07-27 02:43:19 +1000

    task 1

--- src/test/java/com/jpmc/midascore/TaskOneTests.java
+++ src/test/java/com/jpmc/midascore/TaskOneTests.java
@@ -5,7 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "general.kafka-topic=test-topic")
 class TaskOneTests {
     static final Logger logger = LoggerFactory.getLogger(TaskOneTests.class);
 
